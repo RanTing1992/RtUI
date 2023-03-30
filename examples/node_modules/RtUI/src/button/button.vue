310db847ba7d610530b362adbd2f5eade48ebf49
ranting <593087168@qq.com>
2023-03-30 16:50:44 +0800

    button组件

--- examples/node_modules/RtUI/src/button/button.vue
+++ examples/node_modules/RtUI/src/button/button.vue
@@ -1,3 +1,19 @@
 <template>
-    <button>测试按钮</button>
+  <button @click="increment">
+    {{ state.count }}
+  </button>
 </template>
+
+<script setup>
+import { reactive ,ref} from 'vue'
+
+const state = reactive({ count: 0 })
+const s = reactive(false)
+const num = ref(0)
+function increment() {
+    console.log(state,'这个不用value可以访问吗')
+    console.log(num.value,'这个呢')
+    console.log(s,'这个呢77')
+    state.count++
+}
+</script>

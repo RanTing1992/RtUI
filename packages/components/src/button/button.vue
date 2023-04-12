0bc8b8630e2933d716d66bce9afd8cd04ca65792
ranting <593087168@qq.com>
2023-04-12 18:28:23 +0800

    btn组件提交

--- packages/components/src/button/button.vue
+++ packages/components/src/button/button.vue
@@ -2,18 +2,25 @@
   <button @click="increment">
     {{ state.count }}
   </button>
+  <p v-show="isShow">{{isShow?'展示':'不展示'}}</p>
+
+  <!-- <div @click="doEvent('hello',$event)">事件处理</div> -->
+  <div @click="(event=>doEvent('hello',event))">事件处理</div>
+  <!-- 不传递参数的时候 @click="event" 和@click="event()" 好像没什么区别 ，
+  看下vue的模板编译，处理事件这块；方法事件处理器/内联事件处理器-->
 </template>
 
 <script setup>
 import { reactive ,ref} from 'vue'
 
 const state = reactive({ count: 0 })
-const s = reactive(false)
+const isShow = ref(false)
 const num = ref(0)
 function increment() {
-    console.log(state,'这个不用value可以访问吗')
-    console.log(num.value,'这个呢')
-    console.log(s,'这个呢77')
+    isShow.value = !isShow.value
     state.count++
+}
+function doEvent(msg,event){
+    console.log('1111111111',msg,event)
 }
 </script>
